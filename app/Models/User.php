5a60cc27ee53d8e2c9211cfc542b82d0026d833f
天尽头流浪 <1270469522@qq.com>
2021-11-12 07:50:58 +0000

    用户个人中心页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function gravatar($size = '100')
+    {
+        // $hash = md5(strtolower(trim($this->attributes['email'])));
+        // return "http://www.gravatar.com/avatar/$hash?s=$size";
+        return 'https://cdn.learnku.com/uploads/avatars/46657_1565072467.jpeg!/both/100x100';
+    }
 }
